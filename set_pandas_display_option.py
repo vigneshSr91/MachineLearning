5ecd8208facec2cd672afe7aebf8e99f2547a92c
Vignesh Subramanian <74016485+vigneshSr91@users.noreply.github.com>
2020-12-26 12:11:10 +0530

    Create set_pandas_display_option.py

---
+++ set_pandas_display_option.py
@@ -0,0 +1,3 @@
+pd.set_option('max_colwidth', 1000) # show upto 1000 charcters in each cell
+pd.set_option('max_rows', 20) # show upto 20 dataframe rows
+pd.set_option('max_columns', 1000) # show upto 1000 columns
